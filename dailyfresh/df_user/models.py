73e722bd91cfd606bb7856cb1780c6bf6be07b69
neil080320 <1426567189@qq.com>
2018-08-25 10:24:31 +0800

    创建df_user模块

---
+++ dailyfresh/df_user/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+# Create your models here.
+
+class UserInfo(models.Model):
+    uname = models.CharField(max_length=20)
+    upwd = models.CharField(max_length=40)
+    def __str__(self):
+        return "%d" % self.pk
