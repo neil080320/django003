9a26bbcd4fa1deea9ce538ed9ab96029eabf20d4
neil080320 <1426567189@qq.com>
2018-08-25 10:28:24 +0800

    删除模型类的str方法

--- dailyfresh/df_user/models.py
+++ dailyfresh/df_user/models.py
@@ -5,5 +5,4 @@ from django.db import models
 class UserInfo(models.Model):
     uname = models.CharField(max_length=20)
     upwd = models.CharField(max_length=40)
-    def __str__(self):
-        return "%d" % self.pk
+   
